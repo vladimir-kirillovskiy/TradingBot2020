546dfa540233d1a7c26a2a032a5c0c6027156bf3
Daniil <daniil_demichev@mail.ru>
2020-07-12 10:46:15 +0300

    Улучшенная версия визуализации

--- visualization/candlestick.py
+++ visualization/candlestick.py
@@ -1,42 +1,38 @@
+import plotly.graph_objects as go
 import datetime
-import datetime as dt
-import pandas_datareader as web
-import matplotlib.pyplot as plt
-import matplotlib.dates as mdates
-from mpl_finance import candlestick_ohlc
-
 import config, requests, json, pandas as pd
 
-minute_bars_url = config.BARS_URL + '/day?symbols=AAPL&limit=10'
+TICKERS = 'AAPL' # Указать интересующие тикеры, если нужно несколько, то перечислить через запятую (Пока работает
+# только для 1)
+LIMIT = 100 # Количество интервалов для отображения
+
+# Получаем данные с Alpaca о текущем состоянии
+
+minute_bars_url = config.BARS_URL + '/minute?symbols={}&limit={}'.format(TICKERS,LIMIT)
 r = requests.get(minute_bars_url, headers = config.HEADERS)
 data = json.dumps(r.json(), indent=4)
+
+# Записываем их в файл, затем считываем в формате json
 with open('data.txt', 'w') as output:
     output.write(data)
 with open('data.txt') as input:
     data = json.load(input)
-data_dict = {'t':[], 'o':[], 'h':[], 'l':[], 'c':[]}
 
-for elem in data['AAPL']:
+data_dict = {'t':[], 'o':[], 'h':[], 'l':[], 'c':[]} # Словарь для создания DataFrame
+
+# На основании информации заполняем data_dict, затем создаем DataFrame на его основе
+
+for elem in data[TICKERS]:
     data_dict['t'].append(datetime.datetime.fromtimestamp(elem['t']))
     data_dict['o'].append(elem['o'])
     data_dict['h'].append(elem['h'])
     data_dict['l'].append(elem['l'])
     data_dict['c'].append(elem['c'])
 data = pd.DataFrame(data_dict)
-data['t'] = data['t'].map(mdates.date2num)
-print(data)
 
+# Создание графика Candlestick по данным из DataFrame
 
-ax = plt.subplot()
-ax.grid(True)
-ax.set_axisbelow(True)
-ax.set_title('AAPL Price', color = 'white')
-ax.xaxis_date()
-ax.set_facecolor('black')
-ax.figure.set_facecolor('#121212')
-ax.tick_params(axis = 'x', colors = 'white')
-ax.tick_params(axis = 'y', colors = 'white')
-
-candlestick_ohlc(ax, data.values, width=0.5, colorup='#00ff00')
-
-plt.show()
+candlestick = go.Candlestick(x=data['t'], open = data['o'], high= data['h'], low= data['l'], close= data['c'])
+figure = go.Figure(data=[candlestick])
+figure.layout.xaxis.type = 'category'
+figure.show()
