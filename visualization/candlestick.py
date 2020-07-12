70245df1fe418b3b06e556fc6ffe88f9444cce0d
Daniil <daniil_demichev@mail.ru>
2020-07-12 10:14:59 +0300

    Данные получаются с Alpaca, но если ставить интервал 1 или 5 минут, то график становится нечитаемый

--- visualization/candlestick.py
+++ visualization/candlestick.py
@@ -1,19 +1,31 @@
-from config import *
+import datetime
 import datetime as dt
 import pandas_datareader as web
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
 from mpl_finance import candlestick_ohlc
 
-start = dt.datetime(2020, 6, 11)
-end = dt.datetime.now()
+import config, requests, json, pandas as pd
 
-data = web.DataReader("AAPL", 'yahoo', start, end)
+minute_bars_url = config.BARS_URL + '/day?symbols=AAPL&limit=10'
+r = requests.get(minute_bars_url, headers = config.HEADERS)
+data = json.dumps(r.json(), indent=4)
+with open('data.txt', 'w') as output:
+    output.write(data)
+with open('data.txt') as input:
+    data = json.load(input)
+data_dict = {'t':[], 'o':[], 'h':[], 'l':[], 'c':[]}
 
-data = data[['Open', 'High', 'Low', 'Close']]
+for elem in data['AAPL']:
+    data_dict['t'].append(datetime.datetime.fromtimestamp(elem['t']))
+    data_dict['o'].append(elem['o'])
+    data_dict['h'].append(elem['h'])
+    data_dict['l'].append(elem['l'])
+    data_dict['c'].append(elem['c'])
+data = pd.DataFrame(data_dict)
+data['t'] = data['t'].map(mdates.date2num)
+print(data)
 
-data.reset_index(inplace=True)
-data['Date'] = data['Date'].map(mdates.date2num)
 
 ax = plt.subplot()
 ax.grid(True)
