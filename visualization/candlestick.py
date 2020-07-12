e4fd4c5adfa11e9065cf9ba71493a440cb7db5f3
Daniil <daniil_demichev@mail.ru>
2020-07-12 16:06:52 +0300

    Возможность устанавливать значения для индикаторов средних

--- visualization/candlestick.py
+++ visualization/candlestick.py
@@ -2,14 +2,17 @@ import plotly.graph_objects as go
 import datetime
 import config, requests, json, pandas as pd
 
-TICKERS = 'AAPL' # Указать интересующие тикеры, если нужно несколько, то перечислить через запятую (Пока работает
+TICKERS = 'DIS'  # Указать интересующие тикеры, если нужно несколько, то перечислить через запятую (Пока работает
 # только для 1)
-LIMIT = 100 # Количество интервалов для отображения
-
+LIMIT = 500  # Количество интервалов для отображения
+# Настройка показателей индикаторов
+n1 = 10
+n2 = 50
+nmax = max(n1, n2)
 # Получаем данные с Alpaca о текущем состоянии
 
-minute_bars_url = config.BARS_URL + '/day?symbols={}&limit={}'.format(TICKERS,LIMIT)
-r = requests.get(minute_bars_url, headers = config.HEADERS)
+minute_bars_url = config.BARS_URL + '/minute?symbols={}&limit={}'.format(TICKERS, LIMIT)
+r = requests.get(minute_bars_url, headers=config.HEADERS)
 data = json.dumps(r.json(), indent=4)
 
 # Записываем их в файл, затем считываем в формате json
@@ -18,7 +21,7 @@ with open('data.txt', 'w') as output:
 with open('data.txt') as input:
     data = json.load(input)
 
-data_dict = {'t':[], 'o':[], 'h':[], 'l':[], 'c':[]} # Словарь для создания DataFrame
+data_dict = {'t': [], 'o': [], 'h': [], 'l': [], 'c': []}  # Словарь для создания DataFrame
 
 # На основании информации заполняем data_dict, затем создаем DataFrame на его основе
 
@@ -29,52 +32,37 @@ for elem in data[TICKERS]:
     data_dict['l'].append(elem['l'])
     data_dict['c'].append(elem['c'])
 data = pd.DataFrame(data_dict)
-data_draw = data.tail(LIMIT-49)
+data_draw = data.tail(LIMIT - nmax + 1)
 
 # Создание графика Candlestick по данным из DataFrame
 
-candlestick = go.Candlestick(x=data_draw['t'], open = data_draw['o'], high= data_draw['h'], low= data_draw['l'], close= data_draw['c'])
+candlestick = go.Candlestick(x=data_draw['t'], open=data_draw['o'], high=data_draw['h'], low=data_draw['l'],
+                             close=data_draw['c'])
 figure = go.Figure(data=[candlestick])
 figure.layout.xaxis.type = 'category'
 
-# Настройка первого индикатора
 
-x10 = []
-y10 = []
-for i in range(50, LIMIT+1):
-    y10.append(data.loc[i-10:i-1]['c'].mean())
-    x10.append(data.iloc[i-1]['t'])
-trace1 = {
-  "x": x10,
-  "y": y10,
-  "line": {
-    "color": "#3859ff",
-    "width": 2
-  },
-  "mode": "lines",
-  "name": "MA10",
-  "type": "scatter",
-}
+# Функция для создания индикаторов и их отображения
+def create_indicator(n, nmax, color):
+    x = []
+    y = []
+    for i in range(nmax, LIMIT + 1):
+        y.append(data.loc[i - n:i - 1]['c'].mean())
+        x.append(data.iloc[i - 1]['t'])
+    trace = {
+        "x": x,
+        "y": y,
+        "line": {
+            "color": color,
+            "width": 2
+        },
+        "mode": "lines",
+        "name": "MA" + str(n),
+        "type": "scatter",
+    }
+    return trace
 
-# Настройка второго индикатора
 
-x50 = []
-y50 = []
-for i in range(50, LIMIT+1):
-    y50.append(data.loc[i-50:i-1]['c'].mean())
-    x50.append(data.iloc[i-1]['t'])
-trace2 = {
-  "x": x50,
-  "y": y50,
-  "line": {
-    "color": "#385965",
-    "width": 2
-  },
-  "mode": "lines",
-  "name": "MA50",
-  "type": "scatter",
-}
-
-figure.add_trace(trace1)
-figure.add_trace(trace2)
+figure.add_trace(create_indicator(n1, nmax, '#3859ff'))
+figure.add_trace(create_indicator(n2, nmax, '#000000'))
 figure.show()
