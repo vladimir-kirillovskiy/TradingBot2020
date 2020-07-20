e805f2722c14c3bdbaa6538a6875d2bf8d6036a1
BabyFaceNelson <47722141+BabyFaceNelson@users.noreply.github.com>
2020-07-20 14:48:03 +0300

    Update New_Base.py

--- New_Base.py
+++ New_Base.py
@@ -2,16 +2,19 @@ import alpaca_trade_api as tradeapi
 from alpaca_trade_api import StreamConn
 import time
 wait = 60
-api = tradeapi.REST('API_KEY', 'SEKRET_KEY')
+api = tradeapi.REST('API_KEY', 'SEKRET_KEY', 'https://paper-api.alpaca.markets',api_version='v2')
 
 print("Введите акцию для отслеживания: ")
 unit = input().upper()
 
-while True:
+while True:    
     barset = api.get_barset(unit, '1Min', limit=5)
     aapl_bars = barset[unit]
+    aapl_asset = api.get_asset(unit)
     price = aapl_bars[-1].c
     #print(price)
+    if (price < 100 and aapl_asset.tradable):
+        api.submit_order(symbol=unit,qty=1,side='buy',type='limit',time_in_force='gtc',limit_price=100)
     time.sleep(wait)
 
 
