2805e922447f454832782769d1a07002faa851b2
BabyFaceNelson <47722141+BabyFaceNelson@users.noreply.github.com>
2020-07-20 03:49:28 +0300

    Update New_Base.py

--- New_Base.py
+++ New_Base.py
@@ -1,16 +1,19 @@
 import alpaca_trade_api as tradeapi 
 from alpaca_trade_api import StreamConn
-
-
-api = tradeapi.REST('API_KEY', 'API_SEKRET_KEY')
+import time
+wait = 60
+api = tradeapi.REST('API_KEY', 'SEKRET_KEY')
 
 print("Введите акцию для отслеживания: ")
 unit = input().upper()
+
 while True:
     barset = api.get_barset(unit, '1Min', limit=5)
     aapl_bars = barset[unit]
-
     price = aapl_bars[-1].c
     #print(price)
+    time.sleep(wait)
+
+
 
 
