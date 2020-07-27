566a71f854de7df3b3ec6e042d02bcb6d743ba6d
BabyFaceNelson <47722141+BabyFaceNelson@users.noreply.github.com>
2020-07-27 17:12:10 +0300

    Update New_Base.py

--- New_Base.py
+++ New_Base.py
@@ -2,7 +2,7 @@ import alpaca_trade_api as tradeapi
 from alpaca_trade_api import StreamConn
 import time, websocket, json 
 import config
-from candlestick import get_dataframe, get_last_price
+from candlestick import get_dataframe, get_last_price, check_indicator
 from Risk import risk, risk_buy, risk_sell
 wait = 60
 api = tradeapi.REST('PKI5VSIHBY5QD660GUDG', '2BSNsP8amM0q7eFk0dq/xV4IOHhcYKQpcaWndd4u', 'https://paper-api.alpaca.markets',api_version='v2')
@@ -31,25 +31,16 @@ def on_message(ws, message):
     #print(message)
     df = get_dataframe(unit,100)
     price = get_last_price(df,'c')
-    
+    todo = check_indicator(df,'ma')
+    todo = todo.lower()
     print(price)
-    risks = risk('buy', unit)
+    risks = risk(todo, unit)
     print(risks[1])
     #if (price<10000):
         #api.submit_order(symbol=unit,qty=risks[1],side='buy',type='limit',time_in_force='gtc',limit_price=risks[0])
     #else:
         #print(risks)
-
-
-
-
-def on_close(ws):
-    print("closed connection")
-
-socket = "wss://data.alpaca.markets/stream"
-
-ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
-ws.run_forever()
+        
 
 
 
