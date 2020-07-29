6b5b5296ce6aa604e4551d7e22c8c1826ade91c9
BabyFaceNelson <47722141+BabyFaceNelson@users.noreply.github.com>
2020-07-29 13:58:22 +0300

    Update New_Base.py

--- New_Base.py
+++ New_Base.py
@@ -32,7 +32,7 @@ def on_message(ws, message):
     df = get_dataframe(unit,100)
     price = get_last_price(df,'c')
     todo = check_indicator(df,'ma')
-    todo = todo.lower()
+    
     print(price)
     risks = risk(todo, unit)
     print(risks[1])
@@ -40,7 +40,18 @@ def on_message(ws, message):
         #api.submit_order(symbol=unit,qty=risks[1],side='buy',type='limit',time_in_force='gtc',limit_price=risks[0])
     #else:
         #print(risks)
-        
+
+
+
+
+def on_close(ws):
+    print("closed connection")
+
+socket = "wss://data.alpaca.markets/stream"
+
+ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
+ws.run_forever()
+
 
 
 
