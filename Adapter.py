2fca5954abb6881e6e2c45dbb7306f1c52a059aa
Aleksandra947 <66255906+Aleksandra947@users.noreply.github.com>
2020-07-29 08:30:32 +0300

    Создан файл с функциями Alpaca

---
+++ Adapter.py
@@ -0,0 +1,14 @@
+tradeapi.REST(Key, SecretKey, 'https://paper-api.alpaca.markets', api_version='v2') #Аутентификация
+
+def account_state(Key, SecretKey):  # Состояние счета
+        api = tradeapi.REST(Key, SecretKey, 'https://paper-api.alpaca.markets')
+        account = api.get_account()
+        return account.last_equity
+
+api.list_orders()  # Получение всех открытых заявок
+
+api.replace_order()  # Обновление стоп лосса
+
+api.submit_order()
+
+websocket.WebSocketApp()
