f7eaceddfd62b6c168138cb4aeeb24b43812c830
BabyFaceNelson <47722141+BabyFaceNelson@users.noreply.github.com>
2020-08-12 18:19:15 +0300

    Update stop_loss.py

--- stop_loss.py
+++ stop_loss.py
@@ -3,11 +3,11 @@ import alpaca_trade_api as tradeapi
 
 # аргументом функции является название акции
 
-def stop_loss(action, stock):
+def stop_loss(action, stock, api):
     if action == 'Buy':
-        return stop_loss_buy(stock)
+        return stop_loss_buy(stock, api)
     elif action == 'Sell' :
-        stop_loss_sell(stock)
+        return stop_loss_sell(stock, api)
     elif action == 'Skip' :
         pass
     else :
@@ -15,53 +15,17 @@ def stop_loss(action, stock):
         
         
         
-def stop_loss_buy(stock):
-    api = tradeapi.REST(
-        'PKATONHW8O227OL86EIT', # сюда вставляем  свой key
-        'SSt0X7PItaNTMKKW13ObZ3y9NwCsLPsnNkGxH91X', #cюда вставляем свой secret key
-        'https://paper-api.alpaca.markets'
-    )
-    account = api.get_account()
-    df, time = get_dataframe(stock, 500)
+def stop_loss_buy(stock, api):
+    df, time = get_dataframe(stock, 100)
     atr = get_last_ATR(df)
     price = get_last_price(df, 'c')
-    money_on_account = account.last_equity
-    positions=api.list_positions()
-    ppos, time = get_dataframe(positions, 500)
-    risk=0
-    percent_risk=0.05
-    """for i in range len(api.list_positions()) : #Ошибка в функции len()
-        risk=ppos[i]+risk
-    if risk<=money_on_account*percent_risk:
-        if price < float(money_on_account)*percent:
-            stop = price - 2 * atr
-            return stop
-        else :
-            return 0"""
-       
+    stop = price - 2 * atr
+    return stop
+      
 
-
-def stop_loss_sell(stock):
-    api = tradeapi.REST(
-        'PKATONHW8O227OL86EIT', # сюда вставляем  свой key
-        'SSt0X7PItaNTMKKW13ObZ3y9NwCsLPsnNkGxH91X', #cюда вставляем свой secret key
-        'https://paper-api.alpaca.markets'
-    )
-    account = api.get_account()
-    df, time = get_dataframe(stock, 500)
+def stop_loss_sell(stock, api):
+    df, time = get_dataframe(stock, 100)
     atr = get_last_ATR(df)
     price = get_last_price(df, 'c')
-    money_on_account = account.last_equity
-    percent=0.01
-    positions=api.list_positions()
-    ppos, time = get_dataframe(positions, 500)
-    risk=0
-    percent_risk=0.05
-    """for i in range len(api.list_positions()) :   #Ошибка в функции len()
-        risk=ppos[i]+risk
-    if risk<=money_on_account*percent_risk:
-        if price > float(money_on_account)*percent:
-            stop = price + 2 * atr
-            return stop
-        else :
-            return 0"""
+    stop = price + 2 * atr
+    return stop
