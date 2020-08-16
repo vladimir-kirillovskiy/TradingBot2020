ed2f81f3c7de701637dc26a6a4ffacabafccd297
BabyFaceNelson <47722141+BabyFaceNelson@users.noreply.github.com>
2020-08-16 13:28:00 +0300

    Update Base.py

--- Base.py
+++ Base.py
@@ -12,8 +12,7 @@ api = tradeapi.REST(config.KEY_ID, config.SECRET_Key, config.BASE_URL,api_versio
 
 # Ввод нужной акции для работы
 print("Введите акцию для отслеживания: ")
-unit = input().upper()
-
+#unit = input().lower()
 
 def on_open(ws):
     print("opened")
@@ -41,39 +40,44 @@ def on_close(ws):
     print("closed connection")
 
 def workplace():
+    watch_list = api.get_watchlist('6281dc22-4ecc-47ab-8239-eba886159ffa')
+    for each in watch_list.assets:
+        unit = each['symbol']
+        print('Symbol: ', unit)
     #Получение информации из API ALPACA
-    account = api.get_account()
-    replace_stop_loss(api)
-    # Получение информации из Candlestick
-    df = get_dataframe(unit,100)
-    price = get_last_price(df[0],'c')
-    print('Current price: ', price)
-    todo = check_indicator(df[0],'hhll')
-    print('Action: ', todo)
-    stop_price, qnty = risk(todo, unit,api)
+        account = api.get_account()
+        replace_stop_loss(api)
     
-    print('Stop price: ', stop_price)
-    print('Quantity: ', int(qnty))
-    total = qnty * price
-    money = float(account.buying_power)
-    if total>money:
-        qnty = money/price
-    if (stop_price>0 and qnty>0):
-        try:
-            api.submit_order(
-                symbol=unit,
-                qty=int(qnty),
-                side=todo.lower(),
-                type='market',
-                time_in_force='gtc',
-                order_class='oto',
-                stop_loss={'stop_price':stop_price})
-        except Exception as error:
-            print(error)        
+
+    # Получение информации из Candlestick
+        df = get_dataframe(unit,100)
+        price = get_last_price(df[0],'c')
+        print('Current price: ', price)
+        todo = check_indicator(df[0],'hhll')
+        print('Action: ', todo)
+        stop_price, qnty = risk(todo, unit,api)
+    
+        print('Stop price: ', stop_price)
+        print('Quantity: ', int(qnty))
+        total = qnty * price
+        money = float(account.buying_power)
+        if total>money:
+            qnty = money/price
+        if (stop_price>0 and qnty>0):
+            try:
+                api.submit_order(
+                    symbol=unit,
+                    qty=int(qnty),
+                    side=todo.lower(),
+                    type='market',
+                    time_in_force='gtc',
+                    order_class='oto',
+                    stop_loss={'stop_price':stop_price})
+            except Exception as error:
+                print(error)        
         
     
 socket = "wss://data.alpaca.markets/stream"
 
 ws = websocket.WebSocketApp(socket, on_open=on_open, on_message=on_message, on_close=on_close)
 ws.run_forever()
-
