f0bd7dc075c49108a35040c2074ad809e84effb9
BabyFaceNelson <47722141+BabyFaceNelson@users.noreply.github.com>
2020-08-24 04:17:10 +0300

    Update Base.py

--- Base.py
+++ Base.py
@@ -1,100 +1,68 @@
-import alpaca_trade_api as tradeapi 
-from alpaca_trade_api import StreamConn
-import time, websocket, json
-import os 
-import config
-from replace_stop_loss import replace_stop_loss
-import asyncio
-from Risk import risk
-from candlestick import get_dataframe, get_last_price, check_indicator
-api = tradeapi.REST(config.KEY_ID, config.SECRET_Key, config.BASE_URL,api_version=config.API_VERSION)
-test = 'test'
-n1 = 10
-n2 = 50
-
-def on_open(ws):
-    print("opened")
-    auth_data = {
-        "action": "authenticate",
-        "data": {"key_id": config.KEY_ID, "secret_key": config.SECRET_Key}
-    }
-
-    ws.send(json.dumps(auth_data))
-
-    listen_message = {"action": "listen", "data": {"streams": ["AM.AAPL"]}}
-
-    ws.send(json.dumps(listen_message))
-   
-
-
-def on_message(ws, message):
-    print("received a message")
-    print(message)
-    workplace()
-    
-    
-
-def on_close(ws):
-    print("closed connection")
-
 def workplace():
-    replace_stop_loss(api)
+    clock = api.get_clock()
+    if clock.is_open:
+
+        replace_stop_loss(api)
     
-    watch_list = api.get_watchlists()
+        watch_list = api.get_watchlists()
+        watch_list = watch_list[0].id
+        mylist = api.get_watchlist(watch_list)
     
-    watch_list = watch_list[0].id
-    mylist = api.get_watchlist(watch_list)
-    
-    dif_symbols = len(mylist.assets)
-    print('Number of symbols: ', dif_symbols)
-    for each in mylist.assets:
+        dif_symbols = len(mylist.assets)
+        print('Number of symbols: ', dif_symbols)
+
+        for each in mylist.assets:
 
 
-        unit = each['symbol']
-        print('Symbol: ', unit)
-        account = api.get_account()
+            unit = each['symbol']
+            print('Symbol: ', unit)
+            account = api.get_account()
 
 
-        df = get_dataframe(unit,100)
-        price = get_last_price(df[0],'c')
-        print('Current price: ', price)
-        
-        todo = check_indicator(df[0],'hhll')
-        df[0]['cmean10'] = df[0]['c'].rolling(n1).mean()
-        df[0]['cmean50'] = df[0]['c'].rolling(n2).mean()
-        trend10 = df[0]['cmean10'].iloc[-1]
-        trend50 = df[0]['cmean50'].iloc[-1]
-        if (trend10 > trend50 and todo == 'Buy'):
-            todo = 'Buy'
-        elif (trend10 < trend50 and todo == 'Sell'):
-            todo = 'Sell'
-        else:
-            todo = 'Skip'
-        print('Action: ', todo)
+            df = get_dataframe(unit,100)
+            print(df[0].iloc[-1])
+            price = get_last_price(df[0],'c')
+            print('Current price: ', price)
+
+            todo = check_indicator(df[0],'hhll')
+
+            #df[0]['cmean10'] = df[0]['c'].rolling(n1).mean()
+            #df[0]['cmean50'] = df[0]['c'].rolling(n2).mean()
+            trend10 = df[0].iloc[-1]['cmean100']
+            trend50 = df[0].iloc[-1]['cmean50']
+            if (trend10 > trend50 and todo == 'Buy'):
+                todo = 'Buy'
+            elif (trend10 < trend50 and todo == 'Sell'):
+                todo = 'Sell'
+            else:
+                todo = 'Skip'
+            print('Action: ', todo)
 
 
-        stop_price, qnty = risk(todo, unit,api)
-        qnty = int(qnty)
-        print('Stop price: ', stop_price)
-        print('Quantity: ', int(qnty))
+            stop_price, qnty = risk(todo, unit,api)
+            qnty = int(qnty)
+            print('Stop price: ', stop_price)
+            print('Quantity: ', int(qnty))
 
-        total = qnty * price
-        money = float(account.buying_power)
+            total = qnty * price
+            money = float(account.buying_power)
 
-        if total>money:
-            qnty = int(money/price)
-        if (qnty>0):
-            try:
-                api.submit_order(
-                    symbol=unit,
-                    qty=int(qnty/dif_symbols),
-                    side=todo.lower(),
-                    type='market',
-                    time_in_force='gtc',
-                    order_class='oto',
-                    stop_loss={'stop_price':stop_price})
-            except Exception as error:
-                print(error)        
+            if total>money:
+                qnty = int(money/price)
+            if (qnty>0):
+                try:
+                    api.submit_order(
+                        symbol=unit,
+                        qty=int(qnty/dif_symbols),
+                        side=todo.lower(),
+                        type='market',
+                        time_in_force='gtc',
+                        order_class='oto',
+                        stop_loss={'stop_price':stop_price})
+                except Exception as error:
+                    print(error)
+
+
         
     
 socket = "wss://data.alpaca.markets/stream"
